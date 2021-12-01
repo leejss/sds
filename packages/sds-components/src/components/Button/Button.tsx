82633c081c0423176fdad4a0c525dc534732c9e5
JakeLee <jake.lee@jalsalda.com>
2021-12-01 15:53:56 +0900

    😎

--- packages/sds-components/src/components/Button/Button.tsx
+++ packages/sds-components/src/components/Button/Button.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./Button.scss";
+import cx from "classnames";
 
-export const Button: React.FC = ({ children }) => {
-  return <button className="sds-primary">{children}</button>;
+export interface ButtonProps {
+  role: "primary" | "secondary";
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, role }) => {
+  const classNames = cx(role, "btn");
+  return <button className={classNames}>{children}</button>;
 };
